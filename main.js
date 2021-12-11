0c7c9c2b26075c645f387b73d5c797f54ea1a9dc
Cutekittycat <81061612+Cutekittycat@users.noreply.github.com>
2021-12-11 15:51:10 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects=[];
 video = "";
 Status = "";
 
@@ -13,6 +14,21 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 480, 380)
+      if(Status != "")
+      {
+          objectDetector.detect(video,gotResult);
+          for (i = 0; i < object.length; i++) {
+              document.getElementById("status").innerHTML = "Status : Object Detected";
+              document.getElementById("number_of_objects").innerHTML = "Number of objects detects are : "+ object.length;
+
+              fill("#00FFFF");
+              percent = floor(objects[i].confidence * 100);
+              text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+              noFill();
+              stroke("#FF0000");
+              rect(object[i].x, object[i].y, object[i].width, object[i].height);
+          }
+      }
 }
 
 function start()
@@ -27,4 +43,12 @@ function modelLoaded() {
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results){
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    object = results;
 }
